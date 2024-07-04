01ea30e6648082da671cc1c2b0fe977026d127cf
blueJpg <2238288979@qq.com>
2024-07-04 14:29:27 +0800

    [+] add util lib

--- libs/cutil/target/debug/build/anyhow-369e17d956a69adc/build_script_build-369e17d956a69adc.d
+++
@@ -1,5 +0,0 @@
-/home/blue/learnzone/learn-rust/sollet-project/libs/cutil/target/debug/build/anyhow-369e17d956a69adc/build_script_build-369e17d956a69adc: /home/blue/.cargo/registry/src/index.crates.io-6f17d22bba15001f/anyhow-1.0.86/build.rs
-
-/home/blue/learnzone/learn-rust/sollet-project/libs/cutil/target/debug/build/anyhow-369e17d956a69adc/build_script_build-369e17d956a69adc.d: /home/blue/.cargo/registry/src/index.crates.io-6f17d22bba15001f/anyhow-1.0.86/build.rs
-
-/home/blue/.cargo/registry/src/index.crates.io-6f17d22bba15001f/anyhow-1.0.86/build.rs:
--- libs/cutil/target/debug/libcutil.d
+++
@@ -1,1 +0,0 @@
-/home/blue/learnzone/learn-rust/sollet-project/libs/cutil/target/debug/libcutil.rlib: /home/blue/learnzone/learn-rust/sollet-project/libs/cutil/src/fs.rs /home/blue/learnzone/learn-rust/sollet-project/libs/cutil/src/lib.rs /home/blue/learnzone/learn-rust/sollet-project/libs/cutil/src/number.rs /home/blue/learnzone/learn-rust/sollet-project/libs/cutil/src/str.rs
